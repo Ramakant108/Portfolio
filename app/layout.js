44d1db449fc228f7629e88557d84e8785894768f
Ramakant chaudhari <ramakantchaudhari309@gmail.com>
2024-12-15 11:32:41 +0530

    commited

--- app/layout.js
+++ app/layout.js
@@ -15,12 +15,12 @@ export const metadata = {
     "This is the portfolio of Ramakant Chaudhria. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }){
   return (
     <html lang="en">
-      {/* <head>
-      <link rel="icon" href="favicon.ico" type="image/x-icon"/>
-      </head> */}
+      <head>
+      <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+      </head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
